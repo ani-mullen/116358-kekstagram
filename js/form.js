861d96d28ce126daa463c0908862108423ab7a98
Anna Karpina <ani-mullen@yandex.ru>
2017-02-25 15:59:41 +0300

    Bugfix

--- js/form.js
+++ js/form.js
@@ -1,16 +1,51 @@
 'use strict';
 
 (function () {
+  var uploadForm = document.querySelector('#upload-select-image');
+  var framingForm = document.querySelector('.upload-overlay');
+  var onframingFormClose = null;
   var framingFormClose = document.querySelector('.upload-form-cancel');
   var uploadImageName = document.querySelector('#upload-file');
   var uploadImageLabel = document.querySelector('.upload-file');
   var imagePreview = document.querySelector('.filter-image-preview');
 
+  function toggleWidgets() {
+    uploadForm.classList.toggle('invisible');
+    framingForm.classList.toggle('invisible');
+  }
+
+  // Если нажали на Esc
+  function onKeyDownEsc(evt) {
+    if (window.assist.isDeactivationEvent(evt)) {
+      toggleWidgets();
+    }
+  }
+  // Функция переключения видимости загрузчика и редактора
+  function openFramingForm() {
+    toggleWidgets();
+    document.addEventListener('keydown', onKeyDownEsc);
+  }
+
+  // Функция скрытия редактора
+  function hideFramingForm() {
+    framingForm.classList.add('invisible');
+    uploadForm.classList.remove('invisible');
+    document.removeEventListener('keydown', onKeyDownEsc);
+
+    if (typeof onframingFormClose === 'function') {
+      onframingFormClose();
+    }
+  }
+
+  function focusUploadForm(callback) {
+    onframingFormClose = callback;
+  }
+
   // Обработчик нажатия на форму загрузки
   uploadImageLabel.addEventListener('keydown', function (evt) {
     if ((window.assist.isActivationEvent(evt)) && (evt.target.tagName === 'LABEL')) {
       uploadImageName.click();
-      window.isWidgetVisibility.focusUploadForm(function () {
+      focusUploadForm(function () {
         uploadImageLabel.focus();
       });
     }
@@ -18,7 +53,7 @@
 
   // Обработчик загрузки картинки
   uploadImageName.addEventListener('change', function () {
-    window.isWidgetVisibility.openFramingForm();
+    openFramingForm();
     uploadImageName.value = (''); // очистка названия картинки
     window.createScale(document.querySelector('.upload-resize-controls'), scaleClickHandler); // масштабирование
     window.initializeFilters(document.querySelector('.upload-filter-controls'), filterClickHandler); // переключение фильтров
@@ -35,14 +70,14 @@
 
   // Обработчик клика на крест
   framingFormClose.addEventListener('click', function () {
-    window.isWidgetVisibility.hideFramingForm();
+    hideFramingForm();
     clearValues();
   });
 
   // Обработчик нажатия на крест
   framingFormClose.addEventListener('keydown', function (evt) {
     if (window.assist.isActivationEvent(evt)) {
-      window.isWidgetVisibility.hideFramingForm();
+      hideFramingForm();
       clearValues();
     }
   });
